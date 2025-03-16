f55a1d4f0a64776e85bd2186dd38442af22937d1
bill.lin <zzx123bill@gmail.com>
2025-03-16 22:34:12 +0800

    setup frontend and backend project

---
+++ server-sent-events-backend/src/main/java/com/bill/serversenteventsbackend/ServerSentEventsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bill.serversenteventsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSentEventsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSentEventsBackendApplication.class, args);
+    }
+
+}
---
+++ server-sent-events-backend/src/test/java/com/bill/serversenteventsbackend/ServerSentEventsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.serversenteventsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSentEventsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
